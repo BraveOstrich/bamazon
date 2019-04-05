f9a2f19beb8f7d7396a44146c957e714614c6057
Steve White <stevewhitecodes@gmail.com>
2019-04-05 08:28:11 -0500

    Updating current progress

--- bamazon.js
+++ bamazon.js
@@ -16,23 +16,75 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("Connected to database bamazonDB");
-    start();
+    displayItems();
 });
 
-// Functions 
+// Functions //
 
-function start(){
-    inquirer.prompt({
-        name: "item",
-        type: "input",
-        message: "What item would you like to buy?"
+    // Products & Quantity Table //
+function displayItems(){
+    connection.query("SELECT * FROM products", function(err, res){
+        
+        var productTable = [];
+        for (var i = 0; i < res.length; i++) {
+            productTable.push(
+                {
+                    "Item ID": res[i].item_id,
+                    "Product Name": res[i].product_name,
+                    "Department Name": res[i].department_name,
+                    "Price": res[i].price,
+                    "Stock Quantity": res[i].stock_quantity
+                }
+            );
+          }
+          console.table(productTable);
+          start();
     })
+}
+
+    // Product & Quantity Selection //
+function start(){
+    inquirer.prompt([
+        {
+            name: "itemID",
+            type: "input",
+            message: "What is the item ID of the product you would like to buy?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+        }
+    ])
     .then (function(answer){
+        console.log(answer);
         connection.query(
-            "SELECT * FROM products WHERE product_name=?"
+            "UPDATE products SET ? WHERE ?", 
+        [
             {
-                
+                stock_quantity: stock_quantity - answer.quantity
+            },
+            {
+                item_id: answer.itemID
             }
-        )
+        
+        ],
+        function(err, res){
+             
+
+            var productTable = [];
+        for (var i = 0; i < res.length; i++) {
+            productTable.push(
+                {
+                    "Item ID": res[i].item_id,
+                    "Product Name": res[i].product_name,
+                    "Department Name": res[i].department_name,
+                    "Price": res[i].price,
+                    "Stock Quantity": res[i].stock_quantity
+                }
+            );
+          }
+          console.table(productTable);
+        })
     })
 }
